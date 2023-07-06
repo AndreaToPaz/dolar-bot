9bed0ba8f4d72387d0369871e6bc248b495777f3
Andrea Paz <46249374+AndreaToPaz@users.noreply.github.com>
2023-07-06 16:28:11 -0400

    List with referencial changes and title of each

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini') 
 
+#Constant
+REF_NAMES = ["EUR", "CNY", "TRY", "RUB", "USD"]
 
 def get_html ( url : str ) -> str :
     """
@@ -18,18 +20,37 @@ def get_html ( url : str ) -> str :
         return ( print(e) ) 
 # end def
 
-def find_html_element_by_id ( html : str, html_tag : str, html_id : str ) -> str:
+def find_html_element_by_id ( html : str, html_tag : str, html_id : str ) ->  str :
     """
     Purpose: find an element in the HTML document by tag and id
     """
-    soup = BeautifulSoup ( html.content, "html.parser" )
-    changes = soup.find_all ( html_tag , class_= html_id )
-    return changes
-    
+    try:
+        soup = BeautifulSoup ( html.content, "html.parser" )
+        changes = soup.find_all ( html_tag , class_= html_id )
+        return changes
+    except Exception as e:
+        return ( print(e) ) 
+# end def
+
+def str_list_to_float_list ( changes : list, changes_name : list ) -> list :
+    """
+    Purpose: arg
+    """
+    ref_change_list = []
+    changes_modify = []
+    for i in range ( len ( changes ) ) :
+        #First replace "," to "." to be able to cast the string to a float later
+        changes_modify.append( changes[i].text.replace ( "," , "." ) )  
+        ref_change_list.append( [ changes_name[i] , float( changes_modify[i] ) ] )
+    return  ref_change_list
 # end def
 
 
+# #
+#                                                                  MAIN
+# #
 
 html =  get_html(config ['BancoDeVenezuela'] ['BANK_URL'])
 changes = find_html_element_by_id(html,"div", "col-sm-6 col-xs-6 centrado" )
-print (changes)
+ref_num_lits = str_list_to_float_list(changes,REF_NAMES)
+print(ref_num_lits)
